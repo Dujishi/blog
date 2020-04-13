e2295176ea6fdf6e725840619bcda2071d90f54d
dujishi <dujishi@zto.cn>
2020-04-13 09:59:01 +0800

    deploy

--- assets/js/search.js
+++ assets/js/search.js
@@ -2,7 +2,7 @@ jQuery(function() {
     function adjust_search_box_width() {
         if ($(".post-directory").length) {
             if ($(".post-directory").is(":visible")) {
-                $("#site_search").width(220);
+                $("#site_search").width(280);
             }
         }
         var searchbar_width = $("#site_search").width();
